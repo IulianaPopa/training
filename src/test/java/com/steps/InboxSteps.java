79d2f70f9965f92a48c71a036aa090dc71b38671
farcasandra <andra.farcas@evozon.com>
2014-07-22 15:54:54 +0300

    update inbox page

---
+++ src/test/java/com/steps/InboxSteps.java
@@ -0,0 +1,43 @@
+package com.steps;
+
+import java.text.ParseException;
+
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.steps.ScenarioSteps;
+
+import com.pages.CalendarPage;
+import com.pages.VacationMenuPage;
+
+@SuppressWarnings("serial")
+public class InboxSteps extends ScenarioSteps {
+
+	VacationMenuPage vacationMenuPage;
+	CalendarPage calendarPage;
+
+    @Step
+    public void new_request() {
+    	vacationMenuPage.click_new_request();
+    }
+    
+   
+    
+    @Step
+    public void select_start_date() {
+    	vacationMenuPage.click_startDate();
+    }
+    @Step
+    public void select_end_date() {
+    	vacationMenuPage.click_endDate();
+    }
+   
+    public void select_start_date(int month, int day, int year) throws ParseException {
+
+    	calendarPage.setDate(month, day, year);
+    }
+    
+    @Step
+    public void select_date(int month, int day, int year) throws ParseException {
+    	calendarPage.setDate(month, day, year);
+    }
+    
+}
