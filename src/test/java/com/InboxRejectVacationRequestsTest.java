2faec6f390039f924d4fe5194c9c93e71aec9353
farcasandra <andra.farcas@evozon.com>
2014-07-23 11:30:42 +0300

    updated Inbox Approve/Reject requests

---
+++ src/test/java/com/InboxRejectVacationRequestsTest.java
@@ -0,0 +1,52 @@
+package com;
+
+import java.text.ParseException;
+
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.ManagedPages;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Story;
+import net.thucydides.core.pages.Pages;
+import net.thucydides.junit.runners.ThucydidesRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import com.requirements.Application;
+import com.steps.FreeDaysHistorySteps;
+import com.steps.LoginSteps;
+import com.steps.MyFreeDaysSteps;
+import com.steps.MyRequestsSteps;
+import com.steps.NewVacationRequestSteps;
+import com.steps.InboxSteps;
+import com.steps.ViewVacationsSteps;
+
+@Story(Application.Search.SearchByKeyword.class)
+@RunWith(ThucydidesRunner.class)
+public class InboxRejectVacationRequestsTest {
+
+	@Managed(uniqueSession = true)
+	public WebDriver webdriver;
+
+	@ManagedPages(defaultUrl = "http://192.168.1.68:9090/login")
+	public Pages pages;
+
+	@Steps
+	public LoginSteps loginSteps;
+
+	
+	@Steps
+	InboxSteps inboxSteps;
+	
+
+	@Test
+	public void createSimpleVacation() throws ParseException {
+		loginSteps.login("andra.farcas", "andra");
+		inboxSteps.inbox();
+		inboxSteps.select_request();
+		inboxSteps.reject_request();
+		
+
+	}
+}
--- src/test/java/com/steps/InboxSteps.java
+++ src/test/java/com/steps/InboxSteps.java
@@ -3,15 +3,31 @@ package com.steps;
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
 
+import com.pages.RequestsAssignedToMePage;
 import com.pages.VacationManagementMenuPage;
 
 @SuppressWarnings("serial")
 public class InboxSteps extends ScenarioSteps {
 	VacationManagementMenuPage vacationManagementMenuPage;
-
+	RequestsAssignedToMePage requestsAssignedToMePage;
 	@Step
 	public void inbox() {
 		vacationManagementMenuPage.click_inbox();
 	}
 
+	
+	@Step
+	public void select_request() {
+		requestsAssignedToMePage.click_selectRequest();
+	}
+
+	@Step
+	public void approve_request() {
+		requestsAssignedToMePage.click_approve_button();
+	}
+	
+	@Step
+	public void reject_request() {
+		requestsAssignedToMePage.click_reject_button();
+	}
 }
